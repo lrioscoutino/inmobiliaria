a9b388985f69d163792ca673e54111f409b778a5
Luis Rios Coutino <lrioscoutino@gmail.com>
2024-04-15 20:32:49 -0600

    ADD Thumbnail 2

--- properties/models.py
+++ properties/models.py
@@ -39,43 +39,44 @@ class Property(models.Model):
     def __str__(self):
         return self.description
 
-    # def save(self, *args, **kwargs):
-    #
-    #     if not self.make_thumbnail():
-    #         # set to a default thumbnail
-    #         raise Exception('Could not create thumbnail - is the file type valid?')
-    #
-    #     super(Property, self).save(*args, **kwargs)
-    #
-    # def make_thumbnail(self):
-    #
-    #     image = Image.open(self.image)
-    #     image.thumbnail(75, 75)
-    #
-    #     thumb_name, thumb_extension = os.path.splitext(self.image.name)
-    #     thumb_extension = thumb_extension.lower()
-    #
-    #     thumb_filename = thumb_name + '_thumb' + thumb_extension
-    #
-    #     if thumb_extension in ['.jpg', '.jpeg']:
-    #         FTYPE = 'JPEG'
-    #     elif thumb_extension == '.gif':
-    #         FTYPE = 'GIF'
-    #     elif thumb_extension == '.png':
-    #         FTYPE = 'PNG'
-    #     else:
-    #         return False  # Unrecognized file type
-    #
-    #     # Save thumbnail to in-memory file as StringIO
-    #     temp_thumb = BytesIO()
-    #     image.save(temp_thumb, FTYPE)
-    #     temp_thumb.seek(0)
-    #
-    #     # set save=False, otherwise it will run in an infinite loop
-    #     self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
-    #     temp_thumb.close()
-    #
-    #     return True
+    def save(self, *args, **kwargs):
+
+        if not self.make_thumbnail():
+            # set to a default thumbnail
+            raise Exception('Could not create thumbnail - is the file type valid?')
+
+        super(Property, self).save(*args, **kwargs)
+
+    def make_thumbnail(self):
+
+        image = Image.open(self.image)
+        thumbnail_size = 75, 75
+        image.thumbnail(thumbnail_size)
+
+        thumb_name, thumb_extension = os.path.splitext(self.image.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+
+        if thumb_extension in ['.jpg', '.jpeg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension == '.gif':
+            FTYPE = 'GIF'
+        elif thumb_extension == '.png':
+            FTYPE = 'PNG'
+        else:
+            return False  # Unrecognized file type
+
+        # Save thumbnail to in-memory file as StringIO
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        # set save=False, otherwise it will run in an infinite loop
+        self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
 
 
 class PropertyBuy(models.Model):
